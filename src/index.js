94920b61dba53f1de0c1501c2d2cedea2db8ffff
AlexEgupov <alex.egupov7@gmail.com>
2022-06-29 09:37:10 +0300

    feat calc.js lesson24

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import smoothScroll from './modules/smoothScroll';
 import inputCheck from './modules/inputCheck';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
-timer('28 june 2022');
+timer('30 june 2022');
 menu();
 modal();
 smoothScroll();
 inputCheck();
 tabs();
 slider('.portfolio-content', '.portfolio-item', '.portfolio-dots');
+calc(100);
